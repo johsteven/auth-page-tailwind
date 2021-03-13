b564729bd6edce314efde3198192879d68ea43c9
johsteven <johsteven222004@gmail.com>
2021-03-14 02:12:42 +0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,12 +4,8 @@ const elQSA = el => document.querySelectorAll(el);
 const on = (el, event, value) => el.addEventListener(event, value);
 
 // variables
-const closeBtn = elQS('.close');
 
 // functions
-function closeBtnEvent(e) {
-    e.target.parentElement.remove();
-}
 
 // main
 window.addEventListener('load', init);
@@ -21,6 +17,4 @@ function init() {
 
 function load() {}
 
-function eventListener() {
-    on(closeBtn, 'click', closeBtnEvent)
-}
+function eventListener() {}
